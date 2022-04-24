69e6c84c7836a577887c910d3c3f821434d23226
Alexey <alexeyradomskiy98@gmail.com>
2022-04-24 21:25:45 +0300

    Add Services

---
+++ VkNewsFeed/Services/API.swift
@@ -0,0 +1,16 @@
+//
+//  API.swift
+//  VkNewsFeed
+//
+//  Created by Alexey on 24.04.2022.
+//
+
+import Foundation
+
+struct API {
+    static let scheme = "https"
+    static let host = "api.vk.com"
+    static let version = "5.131"
+    
+    static let newsFeed = "/method/users.get"
+}
---
+++ VkNewsFeed/Services/AuthService.swift
@@ -0,0 +1,72 @@
+//
+//  AuthService.swift
+//  VkNewsFeed
+//
+//  Created by Alexey on 24.04.2022.
+//
+
+import Foundation
+import VKSdkFramework
+
+protocol AuthServiceDelegate: AnyObject {
+    func authServiceShouldShow(viewController: UIViewController)
+    func authServiceSignIn()
+    func authServiceSignInDidFail()
+}
+
+class AuthService: NSObject, VKSdkDelegate, VKSdkUIDelegate {
+    
+    private let appId = "8147457"
+    private let vkSdk: VKSdk
+    
+    override init() {
+        vkSdk = VKSdk.initialize(withAppId: appId)
+        super.init()
+        print("VKSdk.initialize")
+        vkSdk.register(self)
+        vkSdk.uiDelegate = self
+    }
+    
+    weak var delegate: AuthServiceDelegate?
+    
+    var token: String? {
+        VKSdk.accessToken().accessToken
+    }
+    
+    func wakeUpSession() {
+        let scope = ["offline"]
+        VKSdk.wakeUpSession(scope) { [delegate] (state, error) in
+            switch state {
+            case .initialized:
+                print("initialized")
+                VKSdk.authorize(scope)
+            case .authorized:
+                print("authorized")
+                delegate?.authServiceSignIn()
+            default:
+                delegate?.authServiceSignInDidFail()
+            }
+        }
+    }
+    
+    func vkSdkAccessAuthorizationFinished(with result: VKAuthorizationResult!) {
+        print(#function)
+        if result.token != nil {
+            delegate?.authServiceSignIn()            
+        }
+    }
+    
+    func vkSdkUserAuthorizationFailed() {
+        print(#function)
+        delegate?.authServiceSignInDidFail()
+    }
+    
+    func vkSdkShouldPresent(_ controller: UIViewController!) {
+        print(#function)
+        delegate?.authServiceShouldShow(viewController: controller)
+    }
+    
+    func vkSdkNeedCaptchaEnter(_ captchaError: VKError!) {
+        print(#function)
+    }
+}
